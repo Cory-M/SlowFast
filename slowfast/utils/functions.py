53b58b5fecb131f92ecb28375cb69389972d163f
Chundi Liu <chundiliu@cs.toronto.edu>
2020-05-28 02:25:03 -0400

    fix shape for single pathway

--- slowfast/utils/functions.py
+++ slowfast/utils/functions.py
@@ -3,13 +3,11 @@ import pdb
 
 def flatten(tensor, cfg):
 	CF, H = cfg.DATA.CHUNK_FRAMES, cfg.DATA.TRAIN_CROP_SIZE
-	if isinstance(tensor, (list,)):
-		tensor[0] = tensor[0].view(-1, cfg.DATA.INPUT_CHANNEL_NUM[0], 
-					CF // cfg.SLOWFAST.ALPHA, H, H)
-		tensor[1] = tensor[1].view(-1, cfg.DATA.INPUT_CHANNEL_NUM[1],
-					CF, H, H)
+	if len(tensor) > 1:
+		tensor[0] = tensor[0].view(-1, cfg.DATA.INPUT_CHANNEL_NUM[0], CF // cfg.SLOWFAST.ALPHA, H, H)
+		tensor[1] = tensor[1].view(-1, cfg.DATA.INPUT_CHANNEL_NUM[1], CF, H, H)
 	else:
-		tensor = tensor.view(-1, cfg.DATA.INPUT_CHANNEL_NUM, CF, H, H)
+		tensor[0] = tensor[0].view(-1, cfg.DATA.INPUT_CHANNEL_NUM[0], CF, H, H)
 	return tensor
 			
 def unflatten(tensor, cfg):
