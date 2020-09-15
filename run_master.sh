f1ddfd3349e6bb73af4da2d5d7b766c25690ad58
keyu <keyu@layer6.ai>
2020-09-15 16:24:41 -0400

    multi-node run.sh set up

---
+++ run_master.sh
@@ -0,0 +1,7 @@
+python tools/run_net.py \
+		   --cfg $1 \
+		   --init_method 'tcp://10.215.241.105:7017' \
+		   --num_shards 2 \
+		   NUM_GPUS $2 \
+		   TRAIN.BATCH_SIZE $((16*$2)) \
+		   DATA.PATH_TO_DATA_DIR /home/keyu/keyu/data/videos
