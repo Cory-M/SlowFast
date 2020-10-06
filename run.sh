7bd4331b0698e2dcad01ac61a96d917c6cbf0956
Chundi Liu <chundiliu@cs.toronto.edu>
2020-10-06 16:37:37 -0400

    epic cvrl_sbn training & testing is not working yet

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ python tools/run_net.py \
 		   --cfg $1 \
 		   NUM_GPUS $2 \
 		   TRAIN.BATCH_SIZE $((16*$2)) \
-		   DATA.PATH_TO_DATA_DIR /home/keyu/data/videos
+		   DATA.PATH_TO_DATA_DIR /home/layer6/chundi/epic/rgb/
